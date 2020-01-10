17b7c2dc949ddc54b12f02553c99935ea2aa0000
azzlabs <marco@azzlabs.eu>
2020-01-10 18:25:47 +0100

    Edited loop

--- functions.php
+++ functions.php
@@ -33,8 +33,9 @@ class Social_Nav_Walker extends Walker_Nav_Menu {
 	// Displays start of an element. E.g '<li> Item Name'
     // @see Walker::start_el()
     function start_el(&$output, $item, $depth = 0, $args = [], $id = 0) {
-		$output .= '<li class="' . implode(' ', array_slice($item->classes, 1)) . '"><a href="' . $item->url . '" class="icon ' .  $item->classes[0] . '" title="' . $item->title . '">'
-			. $item->description . '</a>';
+		if (isset($item->classes))
+			$output .= '<li class="' . implode(' ', array_slice($item->classes, 1)) . '"><a href="' . $item->url . '" class="icon ' .  $item->classes[0] . '" title="' . $item->title . '">'
+				. $item->description . '</a>';
     }
 }
 ?>
--- loop.php
+++ loop.php
@@ -1,30 +1,38 @@
-<?php
-    if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" class="post">
-
-		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="post-thumbnail">
-                <img src="<?php echo get_the_post_thumbnail_url($post_id); ?>" alt="<?php the_title(); ?>" />
-			</a>
-		<?php endif; ?>
-		<!-- ./post thumbnail -->
+	<article id="post-<?php the_ID(); ?>" class="article-content">
 
 		<div class="text-holder">
+            <!-- post thumbnail -->
+            <?php if (!(is_page() || is_single()) && has_post_thumbnail()) : // Check if thumbnail exists ?>
+                <div class="post-thumbnail">
+                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="post-thumbnail-link">
+                        <img src="<?php echo get_the_post_thumbnail_url($post_id); ?>" alt="<?php the_title(); ?>" />
+                    </a>
+                </div>
+            <?php endif; ?>
+            <!-- ./post thumbnail -->
+        
             <div class="post-header">
                 <?php if (!is_page()) { ?>
                 <div class="post-meta">
-                    <time class="post-date"><?php the_time(get_option('date_format')); ?></time>
+                    <span class="posted-calendar">
+                        <i class="fa fa-calendar"></i> <a href="#"><time class="post-date"><?php the_time(get_option('date_format')); ?></time></a>&nbsp;
+                    </span>
+                    <span class="article-categories">
+                        <i class="fa fa-archive"></i> <?php the_category(', '); ?>
+                    </span>
+                    <span class="article-tags">
+                        <?php the_tags(' <i class="fa fa-tag"></i> ', ', '); ?>
+                    </span>
+                    <!--span class="comments-link">
+                        <?php if (comments_open( get_the_ID() ) ): ?><a href="#"><i class="fa fa-comment"></i></a> <?php comments_popup_link( __( 'Lascia un commento', 'templated_spatial' ), __( 'Un commento', 'templated_spatial' ), __( '% commenti', 'templated_spatial' )); endif; ?>
+                    </span-->
                 </div>
                 <!-- ./post-meta -->
                 <?php } ?>
-                <?php if (is_archive()) { ?>
-                    <h2 class="post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                <?php } else { ?>
-                    <h2 class="post-title"><?php the_title(); ?></h2>
-                <?php } ?>
+                <h2 class="post-title"><?php the_title(); ?></h2>
             </div>
             <!-- ./post-header -->
             <div class="post-content">
@@ -34,20 +42,19 @@
             <footer class="post-footer">
                 <?php if (is_archive()) { ?>
                     <p class="read-more">
-                        <a href="<?php the_permalink() ?>"><?php _e( 'Continua a leggere', 'templated_spatial' ); ?> <i class="fa fa-angle-double-right"></i></a>
+                        <a href="<?php the_permalink() ?>"><?php _e( 'Continua a leggere', 'templated_spatial' ); ?> &#187;</a>
                     </p>
-                <?php } ?>
-                <?php
-                    edit_post_link('<i class="fa fa-pencil"></i> ' . __( 'Modifica', 'templated_spatial' ), 
-                    '<span class="edit-link">', '</span>', null, 'post-edit-link' );
+                <?php } 
+                    edit_post_link(__( 'Modifica', 'templated_spatial' ), '<span class="edit-link">', '</span>', null, 'post-edit-link' );
                 ?>
             </footer>
             <!-- ./post-footer -->
+            <div class="clearfix"></div>
         </div>
         <!-- ./text-holder -->
 
 	</article>
-	<!-- ./article -->
+	<!-- /article -->
 
 <?php endwhile; ?>
 
@@ -55,8 +62,8 @@
 
 	<!-- article -->
 	<article>
-		<h2><?php _e('Nessun articolo inserito.', 'templated_spatial'); ?></h2>
+		<h2><?php _e( 'Nessun articolo inserito.', 'templated_spatial' ); ?></h2>
 	</article>
-	<!-- ./article -->
+	<!-- /article -->
 
 <?php endif; ?>
